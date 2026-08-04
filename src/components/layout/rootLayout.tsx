3f4107b646fa67b74311245a019e6d00656f5117
Santiago Mustafá <santiagomustafa3@gmail.com>
2026-08-04 17:20:25 -0300

    feat: implement authentication UI with sign-in and sign-up buttons

--- src/components/layout/rootLayout.tsx
+++ src/components/layout/rootLayout.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignUpButton, useAuth } from "@clerk/react";
 import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router";
 import { getMaterias } from "../../db/queries";
@@ -9,6 +10,7 @@ export default function RootLayout() {
   );
   const [loading, setLoading] = useState<boolean>(false);
   const [menu, setMenu] = useState<boolean>(false);
+  const { isSignedIn } = useAuth();
 
   useEffect(() => {
     setLoading(true);
@@ -23,6 +25,46 @@ export default function RootLayout() {
         setLoading(false);
       });
   }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <div className="flex min-h-svh items-center justify-center p-6">
+          <div className="grid h-125 w-200 max-w-full overflow-hidden rounded-2xl border border-border bg-card shadow-sm md:grid-cols-2">
+            {/* Image panel */}
+            <div className="relative hidden md:block">
+              <img
+                src="/authView.jpg"
+                alt="Colinas verdes bajo la luz de la mañana"
+                sizes="400px"
+                className="object-cover"
+              />
+              <div className="absolute inset-0 bg-primary/10" />
+              <div className="absolute bottom-8 left-8 right-8">
+                <p className="font-serif text-xl leading-snug text-primary-foreground text-balance drop-shadow-sm">
+                  Vuelve a lo esencial.
+                </p>
+              </div>
+            </div>
+
+            {/* Form panel */}
+            <div className="flex flex-col justify-center px-8 py-10 sm:px-12">
+              <section className="flex flex-col gap-5">
+                <div className="flex flex-col items-center justify-center gap-5 font-serif text-3xl text-foreground">
+                  <div className="border rounded-xl px-4 py-2 hover:bg-sky-300/40">
+                    <SignInButton> Iniciar Sesión</SignInButton>
+                  </div>
+                  <div className="border rounded-xl px-4 py-2 hover:bg-sky-600/40">
+                    <SignUpButton> Crear Cuenta</SignUpButton>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex h-screen w-full bg-neutral-900 text-neutral-100 font-sans">
