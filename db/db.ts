261501f47b10da092cfe3ba7b136b8cd47795751
Santiago Mustafá <santiagomustafa3@gmail.com>
2026-08-06 12:29:26 -0300

    fix: remove env.ts fila and import env with process.env

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,10 @@
 import * as schema from "@/db/schema/schema";
-import { TURSO_TOKEN, TURSO_URL } from "@/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 const client = createClient({
-  url: TURSO_URL,
-  authToken: TURSO_TOKEN,
+  url: process.env.TURSO_URL!,
+  authToken: process.env.TURSO_TOKEN,
 });
 
 export const db = drizzle(client, { schema });
